d581a1c4b31589e451975dec6006625c3b64b70b
eityans <eityans@gmail.com>
2023-01-09 14:25:17 +0900

    fix

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -27,13 +27,7 @@ class ChirpController extends Controller
      */
     public function create()
     {
-        $validated = $request->validate([
-          'message' => 'required|string|max:255'
-        ]);
-
-        $request->user()->chirps()->create($validated);
-
-        return redirect(route('chirps.index'));
+        //
     }
 
     /**
@@ -44,7 +38,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validated = $request->validate([
+        'message' => 'required|string|max:255'
+      ]);
+
+      $request->user()->chirps()->create($validated);
+
+      return redirect(route('chirps.index'));
     }
 
     /**
