4325de96630e9fb962d4e4bdadd024a736793bc4
eityans <eityans@gmail.com>
2023-01-09 16:40:35 +0900

    delete

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -97,6 +97,8 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        $this->authorize('delete', $chirp);
+        $chirp->delete();
+        return redirect(route('chirps.index'));
     }
 }
